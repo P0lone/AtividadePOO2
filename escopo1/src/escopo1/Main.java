5b7309f442db2de07b1b1bab0b38535bd3f1d429
Gustavo <gutigimenez15@gmail.com>
2024-07-07 23:27:46 -0300

    Correções

--- escopo1/src/escopo1/Main.java
+++ escopo1/src/escopo1/Main.java
@@ -2,7 +2,7 @@ package escopo1;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Usuario gustavo = new Usuario("Gustavo", "Aluno", "20230006414@estudantes.ifpr.edu.br");
+        Usuario gustavo = new Usuario("Gustavo", "Aluno", "meeEmail@gustavo.com");
         Livro senhorDosAneis = new Livro("Senhor dos Aneis", "Num sei", "Nsei", "fantasia", true);
         Emprestimo emprestimo = new Emprestimo("2024-07-05","2024-07-12",gustavo,senhorDosAneis,0);
         Reserva reserva = new Reserva(gustavo, "2024-07-05", senhorDosAneis);
--- escopo1/src/escopo3/Main.java
+++ escopo1/src/escopo3/Main.java
@@ -1,9 +1,11 @@
 package escopo3;
 
 public class Main {
-    Paciente gustavo = new Paciente("Gustavo", null, null);
-    Leito leito061 = new Leito("061", null, true, null);
-    Medico joao = new Medico("João", "Oftalmologista");
-    Exame exame = new Exame(gustavo, "Vista", "2024-07-06", "07:30", null);
-    Consulta consulta = new Consulta(gustavo, joao, "2024-07-05", "07:30", "Oftalmo");
+    public static void main(String[] args) {
+        Paciente gustavo = new Paciente("Gustavo", null, null);
+        Leito leito061 = new Leito("061", null, true, null);
+        Medico joao = new Medico("João", "Oftalmologista");
+        Exame exame = new Exame(gustavo, "Vista", "2024-07-06", "07:30", null);
+        Consulta consulta = new Consulta(gustavo, joao, "2024-07-05", "07:30", "Oftalmo");
+    }
 }
