bf0599e68339cbf2049dc9103b3507a54d46c50b
Gustavo <gutigimenez15@gmail.com>
2024-07-07 23:32:06 -0300

    Correções

--- escopo1/src/escopo2/Main.java
+++ escopo1/src/escopo2/Main.java
@@ -2,9 +2,9 @@ package escopo2;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Voo voo001 = new Voo("Gol","Maringa","Curitiba","2024-007-13","19:00","19:30","R$399,99");
+        Voo voo001 = new Voo("Gol","Maringa","Curitiba","2024-007-13","19:00","19:30",399.99);
         Assento assento42 = new Assento("42",false,"Perto da Janela",voo001);
-        ServicoExtra bagagemDespachada = new ServicoExtra("Bagagem Despachada","R$100");
+        ServicoExtra bagagemDespachada = new ServicoExtra("Bagagem Despachada",100);
         Usuario gustavo = new Usuario("Gustavo","meuEmail@gustavo.com");
         Reserva reserva = new Reserva(gustavo,assento42,"Confirmado",null);
     }
